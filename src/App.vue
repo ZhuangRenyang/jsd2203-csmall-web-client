fa1435a0e75b206b1025a48d6181bfb740f89e28
ZhuangRenyang <996491835@qq.com>
2022-11-15 10:36:21 +0800

    0.1.2 添加了登录页看板娘

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,113 @@
 <template>
   <div id="app">
     <router-view/>
+    <!-- <router-view /> -->
   </div>
 </template>
+<script>
+// export default {
+//   name: 'App',
+//   created() {
+//     window.L2Dwidget
+//         //此处是对点击人物时触发事件的监听
+//         .on('*', (name) => {
+//
+//           console.log('%c 你点击了 ' + '%c -> ' + name, 'background: #222; color: yellow',
+//               'background: #fff; color: #000')
+//         })
+//         //对人物的属性配置
+//         .init({
+//           pluginRootPath: '../live2dw/', //人物文件夹的位置
+//           pluginJsPath: 'lib/',
+//           pluginModelPath: 'live2d-widget-model-shizuku/assets/',
+//           model: {
+//             jsonPath: '../live2dw/live2d-widget-model-shizuku/assets/shizuku.model.json'
+//           },
+//           dialog: {
+//             enable: true, //是否开启对话框
+//             script: {
+//               //每20s，显示一言（调用一言Api返回的句子）
+//               'every idle 20s': '$hitokoto$',
+//               //触摸到class='star'对象,将会展示的文字
+//               'hover .star': '星星在天上而你在我心里 (*/ω＼*)',
+//               //触摸到身体
+//               'tap body': '害羞⁄(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄',
+//               //触摸到头部
+//               'tap face': '~~'
+//             }
+//           },
+//           display: { //人物的属性配置
+//             position: 'left',
+//             width: 350,
+//             height: 800,
+//             hOffset: 200, // 横向偏移
+//             vOffset: -100 // 纵向偏移
+//           },
+//           mobile: {
+//             show: true, //是否在移动端展示
+//             scale: 0.5 //t透明度
+//           }
+//
+//         });
+//   }
+//
+// }
+export default {
+  name: 'app',
+  /*看板娘初始化
+    参数说明
+    model 模型的配置
+       json 文件资源路径，可以支持网络路径，此处使用的是相对路径
+       scale 模型缩放系数
+    display 模型布局
+       position 位置
+       width 宽度
+       height 高度
+       hOffset 水平偏移量
+       vOffset 垂直偏移量
+    mobile 移动端配置
+       show 显示/隐藏
+       scale 缩放比例
+    react 模型矩形框样式
+       opacity 透明度
+       opacityDefault 默认透明度
+       opacityOnHover 鼠标悬浮透明度
+  */
+  created() {
+    setTimeout(() => {
+      window.L2Dwidget.init({
+        pluginRootPath: '../live2dw/',
+        pluginJsPath: 'lib/',
+        pluginModelPath: 'live2d-widget-model-haru_2/assets/', //中间这个haru_2就是你的老婆,想换个老婆,换这个就可以了
+        tagMode: false,
+        debug: false,
+        model: { jsonPath: '../live2dw/live2d-widget-model-haru_2/assets/haru02.model.json' },
+        display: { position: 'right', width: 350, height: 800 },  //调整大小,和位置
+        mobile: { show: true },   //要不要盯着你的鼠标看
+        log: false,
+      })
+    }, 1000)
+  }
+}
+
+
+</script>
+
 <style>
+body {
+  margin: 0;
+  width: 100%;
+}
+
+html {
+  width: 100%;
+  height: 100%;
+}
+
+.app {
+  /*width: 100%;*/
+  /*height: 100%;*/
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,21 +11,21 @@
               router
               :default-active="this.$router.currentRoute.path"
               class="el-menu-vertical-demo">
-<!--              @open="handleOpen"-->
-<!--              @close="handleClose">-->
+            <!--              @open="handleOpen"-->
+            <!--              @close="handleClose">-->
             <el-submenu class="el-submenu" index="/admin/user/list">
               <template slot="title">
                 <i class="el-icon-user"></i>
                 <span>用户管理</span>
               </template>
-                <el-menu-item class="el-menu-item" index="/admin/user/list">用户列表</el-menu-item>
-                <el-menu-item class="el-menu-item" index="/admin/user/add-new">添加用户</el-menu-item>
+              <el-menu-item class="el-menu-item" index="/admin/user/list">用户列表</el-menu-item>
+              <el-menu-item class="el-menu-item" index="/admin/user/add-new">添加用户</el-menu-item>
             </el-submenu>
             <el-submenu class="el-submenu" index="/admin/brand/list">
-            <template slot="title">
-              <i class="el-icon-goods"></i>
-              <span>品牌管理</span>
-            </template>
+              <template slot="title">
+                <i class="el-icon-goods"></i>
+                <span>品牌管理</span>
+              </template>
               <el-menu-item class="el-menu-item" index="/admin/brand/list">品牌列表</el-menu-item>
               <el-menu-item class="el-menu-item" index="/admin/brand/add-new">添加品牌</el-menu-item>
             </el-submenu>
@@ -55,11 +55,13 @@
     </el-container>
   </div>
 </template>
+
 <style>
 
-*{
+* {
   margin: 0;
 }
+
 .el-header {
   /*border: 4px solid transparent;*/
   border-radius: 2px;
@@ -69,34 +71,36 @@
   /*background-image: linear-gradient(to right, #F78FAD, #FDEB82),linear-gradient(90deg, #8F41E9, #578AEF);*/
 }
 
-.el-submenu{
+.el-submenu {
   border-radius: 2px;
   background-clip: padding-box, border-box;
   background-origin: padding-box, border-box;
   background-image: linear-gradient(to right, #F5CCF6, #F1EEF9);
 }
 
-.el-menu-item{
+.el-menu-item {
   border-radius: 2px;
   background-clip: padding-box, border-box;
   background-origin: padding-box, border-box;
   background-image: linear-gradient(to right, #ABC7FF, #C1E3FF);
 }
 
-.layout-body{
+.layout-body {
   position: absolute;
   top: 60px;
   right: 0;
-  bottom:0;
+  bottom: 0;
   left: 0;
 }
-.layout-aside{
+
+.layout-aside {
   border-radius: 2px;
   background-clip: padding-box, border-box;
   background-origin: padding-box, border-box;
   background-image: linear-gradient(to right, #99E5A2, #D4FC78);
 }
-.el-main{
+
+.el-main {
   background-image: url("../assets/bg.png");
   background-size: cover; /* cover是专门用来设置全屏背景的 */
 }
